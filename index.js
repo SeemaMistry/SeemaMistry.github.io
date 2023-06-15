47fb77caeb4c04500d3d62f96b8e675d9457b088
SeemaMistry <seema.m.1996@gmail.com>
2023-06-15 16:38:46 -0400

    feat: add msg alert to submit button and clear contact form after submit

--- index.js
+++ index.js
@@ -28,10 +28,17 @@ function closeMenu() {
 const scriptURL =
   'https://script.google.com/macros/s/AKfycbzLhBe46OElWybICv1VgdHzt572EEesRr9AIJFaCio-PdxIoiAJBb3s69AjdUgtltY9Zg/exec';
 const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById('msg');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then((response) => console.log('Success!', response))
+    .then((response) => {
+      msg.innerHTML = 'Message sent successfully';
+      setTimeout(function () {
+        msg.innerHTML = '';
+      }, 5000);
+      form.reset();
+    })
     .catch((error) => console.error('Error!', error.message));
 });
