1c1365965ec4b7fd1b2e6a252fc67c7695eaee34
SeemaMistry <seema.m.1996@gmail.com>
2023-06-15 16:18:21 -0400

    feat: add google-sheet script for contact form

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.right = '-200px';
 }
+
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycbzLhBe46OElWybICv1VgdHzt572EEesRr9AIJFaCio-PdxIoiAJBb3s69AjdUgtltY9Zg/exec';
+const form = document.forms['submit-to-google-sheet'];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => console.log('Success!', response))
+    .catch((error) => console.error('Error!', error.message));
+});
